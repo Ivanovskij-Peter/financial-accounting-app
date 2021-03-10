232f81b2c1d8be75f9ff1052873b1e56a9346375
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2021-03-10 17:38:32 +0200

    added images

---
+++ src/components/redux/store.js
@@ -0,0 +1,37 @@
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import logger from "redux-logger";
+// import storage from "redux-persist/lib/storage";
+// import {
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+//   persistStore,
+//   persistReducer,
+// } from "redux-persist";
+// const middleware = [
+//   ...getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+//   logger,
+// ];
+// const authPersistConfig = {
+//   key: "auth",
+//   storage,
+//   whitelist: ["token"],
+// };
+// const store = configureStore({
+//   reducer: {
+//     // contacts: contacts,
+//     // auth: persistReducer(authPersistConfig, authReducer),
+//   },
+
+//   middleware,
+//   devTools: process.env.NODE_ENV === "development",
+// });
+// const persistor = persistStore(store);
+// export default { store, persistor };
