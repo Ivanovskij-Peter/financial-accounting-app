07b6df783d8ac69ed08b9f906020f88b50392c61
Natali Baziuchenko <baziuchenko_natali@ukr.net>
2021-03-11 22:12:03 +0200

    Added styles for summary

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import "modern-normalize/modern-normalize.css";
-import { BrowserRouter } from "react-router-dom";
-import store from "./components/redux/store";
-import "./sass/main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import 'modern-normalize/modern-normalize.css';
+import { BrowserRouter } from 'react-router-dom';
+import store from './components/redux/store';
+import './sass/main.scss';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  // <Provider store={store}>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  // </Provider>,
+  document.getElementById('root'),
 );
