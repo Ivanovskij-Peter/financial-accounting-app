3fff95e30f2c75c3fff57e8c0d29c7cfd8ae7cd2
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2021-03-11 20:48:41 +0200

    fix bugs

--- src/components/PrivateRouter.js
+++ src/components/PrivateRouter.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import { connect } from 'react-redux';
 import { Route, Redirect } from "react-router-dom";
-// import { authSelectors } from './redux/auth';
 
 const PrivateRoute = ({
   component: Component,
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import App from "./App";
 // import { Provider } from "react-redux";
 import "modern-normalize/modern-normalize.css";
 import { BrowserRouter } from "react-router-dom";
-import store from "./components/redux/store";
+// import store from "./components/redux/store";
 import "./sass/main.scss";
 
 ReactDOM.render(
   // <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   // </Provider>,
   document.getElementById("root")
 );
