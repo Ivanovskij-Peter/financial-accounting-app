668e15dd51e4e5f651eba9b149f14d3c15023af0
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2021-03-12 16:25:31 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { CSSTransition } from 'react-transition-group';
 import Notification from './components/Notification/Notification';
 import notificationStyles from './components/Notification/notification.module.scss';
 
+import Header from './components/header';
+
 function App() {
   return (
     <div>
-      <Notification />
+      <Header isLogged={true}/>
 
       <h1>Go to work guys!</h1>
     </div>
