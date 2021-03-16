ba6aaeb541b475d623fa66ab2dafc44e968a9401
Julia-itcourse <juliaartemenko93@gmail.com>
2021-03-16 17:16:59 +0100

    add chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 import styles from "./Chart.module.scss";
+import "chartjs-plugin-datalabels";
+
+let chartData = [400, 40, 80, 100, 300];
 
 class Chart extends Component {
   constructor(props) {
@@ -8,22 +11,62 @@ class Chart extends Component {
     this.state = {
       chartData: {
         labels: ["Мясо", "Картоха", "Овощи", "Фрукты", "Рыба"],
-      
-        datasets: [{ 
-          label: "Money",
-          data: [400, 40, 80, 100, 300], 
-          backgroundColor: "yellow"
-        }]
-      }
-    }
-}
+
+        datasets: [
+          {
+            label: "Money",
+            data: chartData,
+            backgroundColor: "orange"
+          }
+        ],
+      },
+    };
+  }
 
   render() {
     return (
       <div className={styles.chart}>
         <Bar
           data={this.state.chartData}
-          options={{ maintainAspectRatio: false }}
+          className={styles.bar}
+          options={{
+            legend: {
+              display: false,
+            },
+            //* Почему не работает то, что внутри объекта вложенного в yAxes? Нужно убрать сетку по вертикали
+            scales: {
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  ticks: {
+                    display: false, // this will remove only the label
+                    suggestedMax: Math.max(...chartData) * 1.05,
+                  },
+                  gridLines: {
+                    drawBorder: false,
+                  },
+                },
+              ],
+            },
+            plugins: {
+              datalabels: {
+                anchor: "end",
+                align: "top",
+                formatter: function (value, context) {
+                  return value + " грн";
+                },
+                font: {
+                  weight: "bold",
+                },
+              },
+            },
+          }}
         />
       </div>
     );
