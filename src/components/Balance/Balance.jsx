9ae7fea934d716df213ee9043bedc063526af8e4
Sasha Skryd <sashaskryd@gmail.com>
2021-03-19 11:45:08 +0200

    new component Report

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import svg from "../../images/sprite.svg";
 import CurrencyInput from "react-currency-input-field";
 import transactionOperation from "../../redux/transaction/transaction-operation";
 import "./balance.scss";
@@ -19,16 +17,6 @@ export default function Balance() {
 
   return (
     <div className="balance">
-      <div className="report">
-        <a href="" className="report_link">
-          <Link to="/reports" className="report_link-deskription">
-            Перейти к отчетам
-          </Link>
-        </a>
-        <svg className="report_icon">
-          <use href={svg + "#chart"} />
-        </svg>
-      </div>
       <div className="balance_container">
         <p className="balance_title">Баланс:</p>
         <div className="balance_form">
@@ -51,7 +39,6 @@ export default function Balance() {
             <span className="balance_botton-text">подтвердить</span>
           </button>
         </div>
-        <p className="balance_calendar">дата</p>
       </div>
     </div>
   );
