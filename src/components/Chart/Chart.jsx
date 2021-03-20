4d41f54a9d3e676f3436a2495b63b65c4edd6389
Julia-itcourse <juliaartemenko93@gmail.com>
2021-03-20 21:25:48 +0100

    adding logic of chart component for desktop

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,73 +1,123 @@
 import React from "react";
-import {useSelector} from "react-redux"
-import styles from "./Chart.module.scss"
-import getReports from "../../redux/reports/reports-selectors"
+import { useSelector } from "react-redux";
+import styles from "./Chart.module.scss";
+import getReports from "../../redux/reports/reports-selectors";
 
+const categoryMap = {
+  products: "Продукты",
+  alcohol: "Алкоголь",
+  entertaiment: "Развлечения",
+  health: "Здоровье",
+  transport: "Транспорт",
+  housing: "Все для дома",
+  equipment: "Техника",
+  utilities: "Коммуналка, связь",
+  hobbies: "Спорт, хобби",
+  education: "Образование",
+  other: "Прочее",
+  "additional-incomes": "ДОП. ДОХОД",
+  salary: "Зп",
+};
 
+export default function Chart(props) {
+  //  console.log("props", props.match.params.category);
+  console.log("props", props);
 
+  const transactionType = props.reportName.toLowerCase();
+  const category = props.match.params.category;
 
-export default function Chart (props) {
-//  console.log("props", props.match.params.category);
- console.log("props", props);
+  const report = useSelector(getReports.getUserReports);
+  console.log("report", report);
+  console.log("transactionType", transactionType);
+  console.log("category", category);
 
-const transactionType = props.reportName.toLowerCase();
-const category = props.match.params.category;
+  const currentObject =
+    transactionType === "расходы" ? report.costs : report.incomes;
 
-const report = useSelector(getReports.getUserReports)
-console.log("report", report);
-console.log("transactionType", transactionType);
+  const categoryRu = categoryMap[category];
 
-const currentObject = transactionType==="расходы" ? report.costs :  report.incomes;
+  console.log("currentObject", currentObject);
+  console.log("CategoryRu", currentObject[categoryRu]);
 
-console.log("currentObject",currentObject );
-    return (
+  const labelToValueObject = currentObject[categoryRu];
+
+  // todo: remove when not required
+  // const labelToValueObject = {
+  //   Свинина: 5000,
+  //   Говядина: 4500,
+  //   Курица: 3200,
+  //   Рыба: 2100,
+  //   Панини: 1800,
+  //   Кофе: 1700,
+  //   Спагетти: 1500,
+  //   Шоколад: 800,
+  //   Маслины: 500,
+  //   Зелень: 300,
+  // };
+
+  const labelToValueArray = [];
+
+  for (let label of Object.keys(labelToValueObject)) {
+    if (label !== "total") {
+      labelToValueArray.push({
+        label: label,
+        value: labelToValueObject[label],
+      });
+    }
+  }
+
+  console.log("labelToValueArray", labelToValueArray);
+
+  labelToValueArray.sort(function (a, b) {
+    const aValue = a["value"];
+    const bValue = b["value"];
+
+    if (bValue > aValue) {
+      return 1;
+    } else if (aValue > bValue) {
+      return -1;
+    }
+    return 0;
+  });
+
+  console.log("labelToValueArray", labelToValueArray);
+
+  const chartCanvasHeight = 328;
+  const heightCoefficient = labelToValueArray.length
+    ? labelToValueArray[0].value / chartCanvasHeight
+    : 1;
+
+  const chartCanvasWidth = 605;
+  const maxBarWidth = 38;
+  let barWidth = chartCanvasWidth / labelToValueArray.length - 25;
+  if (barWidth > maxBarWidth) {
+    barWidth = maxBarWidth;
+  }
+
+  return (
+    <>
       <div className={styles.chart}>
-<ul className= {styles.list}>
-<li className = {styles.item}>
-  <p className = {styles.sum}>20000 грн</p>
-  <p className = {styles.label}>Мясо</p>
-</li>
-<li className = {styles.item}>
-  <p className = {styles.sum}>1000 грн</p>
-  <p className = {styles.label}>Рыба</p>
-</li>
-<li className = {styles.item}>
-  <p className = {styles.sum}>500 грн</p>
-  <p className = {styles.label}>Овощи</p>
-</li>
-<li className = {styles.item}>
-  <p className = {styles.sum}>400 грн</p>
-  <p className = {styles.label}>Фрукты</p>
-</li>
-<li className = {styles.item}>
-  <p className = {styles.sum}>200 грн</p>
-  <p className = {styles.label}>Молоко</p>
-</li>
-
-</ul>
+        <ul className={styles.list}>
+          {labelToValueArray.map(({ label, value }) => {
+            return (
+              <li
+                key={label}
+                className={styles.item}
+                style={{
+                  height: value / heightCoefficient + "px",
+                  width: barWidth + "px",
+                }}
+              >
+                <p className={styles.sum}>{value} грн</p>
+                <p className={styles.label}>{label}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    );
-  
+    </>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import React, { Component } from "react";
 // import { Bar } from "react-chartjs-2";
