1de9c6b149be3fa6d0954707bb6cd3c3a4bb0db0
Galyna Khyzhynska <gal4enock86@gmail.com>
2021-03-21 18:43:03 +0200

    добавляет стили и период

--- src/components/pages/ReportsPage.js
+++ src/components/pages/ReportsPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import Reports from "../Reports";
 import ReportBalance from "../Balance/ReportBalance";
 import Total from "../Reports/Total";
+import BackHome from "../BackHome";
 
 const ReportsPage = () => {
   return (
     <>
+      <BackHome />
       <ReportBalance />
       <Total />
       <Reports />
