395e07ebe9c07c2fe2d68c394efacba3f6da4080
Heorhiivv <georgevbuisness@gmail.com>
2021-03-23 14:26:23 +0200

    adding styles unlogged/logged

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { authSelectors } from "../../redux/auth/index";
+
 import Header from "../header";
 
 import styles from "./Layout.module.scss";
 import { useSelector } from "react-redux";
+
+import kapustaTitle from "../../images/kapusta.png";
 
 const Layout = ({ children }) => {
   const isUserLogged = useSelector(authSelectors.getIsAuthenticated);
@@ -17,7 +20,21 @@ const Layout = ({ children }) => {
           <div className={styles.container}>{children}</div>
         </div>
       ) : (
-        <div className={styles.unloggedTheme}>{children}</div>
+        <div className={styles.unloggedTheme}>
+          <div className={styles.container}>
+            <div className={styles.unloggedGroup}>
+              <div className={styles.heroTitle}>
+                <img
+                  className={styles.kapustaTitle}
+                  src={kapustaTitle}
+                  alt="cabbige"
+                />
+                <p className={styles.smartFinance}>smart finance</p>
+              </div>
+              {children}
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
