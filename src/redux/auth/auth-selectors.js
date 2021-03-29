a3530dcdd8b68b46b6fbc3bc583abe637ccb57d1
Ostap Pluzhnik <opluz@softserveinc.com>
2021-03-29 23:34:43 +0300

    added login request errors handling for user

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -3,6 +3,14 @@ const getUserBalance = (state) => state.operations.balance;
 const getToken = (state) => state.auth.token;
 const getName = (state) => state.auth.user.name;
 const getIsNotVerified = (state) => state.auth.user.isNotVerified;
+const getError = (state) => state.error;
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getIsAuthenticated, getUserBalance, getToken, getName, getIsNotVerified };
+export default {
+  getIsAuthenticated,
+  getUserBalance,
+  getToken,
+  getName,
+  getIsNotVerified,
+  getError,
+};
